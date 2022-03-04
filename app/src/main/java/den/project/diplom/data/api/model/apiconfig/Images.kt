cbf0888b304ad56fabe7067add446cf5cc6a31ad
vazhnik <denis13071981@gmail.com>
2022-03-04 15:57:32 +0300

    add search with trouble ((( + api config data class

---
+++ app/src/main/java/den/project/diplom/data/api/model/apiconfig/Images.kt
@@ -0,0 +1,21 @@
+package den.project.diplom.data.api.model.apiconfig
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Images(
+    @SerializedName("backdrop_sizes")
+    val backdropSizes: List<String>?,
+    @SerializedName("base_url")
+    val baseUrl: String?, // http://image.tmdb.org/t/p/
+    @SerializedName("logo_sizes")
+    val logoSizes: List<String>?,
+    @SerializedName("poster_sizes")
+    val posterSizes: List<String>?,
+    @SerializedName("profile_sizes")
+    val profileSizes: List<String>?,
+    @SerializedName("secure_base_url")
+    val secureBaseUrl: String?, // https://image.tmdb.org/t/p/
+    @SerializedName("still_sizes")
+    val stillSizes: List<String>?
+)
